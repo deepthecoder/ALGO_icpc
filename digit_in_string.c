9335e4a87a4f9bfc9345f37117fe140c0a2002e8
Shubharthi Dey <deysubharthi15@gmail.com>
2017-01-07 14:42:53 +0530

    Update digit_in_string.c

--- digit_in_string.c
+++ digit_in_string.c
@@ -2,6 +2,8 @@
 //Normally isdigit() in C extracts one digit in a time
 //However for example: 213sdvbsjvb33kj7 we would like to extract the digits 213,33,7 at one go
 //To perform the same we have strtol() in  stdlib.h of C
+//However in the same example the ouput would have been 2,1,3,3,3,7 which is not the desired and we have to extend the logic a bit more
+//to check for the ocuurences of digits.
 //Below is a sample program:
 
 #include <stdio.h>
